9549e0f5f3514a7fce6b0a53378727b8df774e47
antonenkokrg <antonenkokrg@gmail.com>
2020-09-24 12:06:40 -0700

    heroku

--- app.py
+++ app.py
@@ -111,14 +111,39 @@ def logout():
 ##############################################################################
 # General user routes:
 
+@app.route('/users')
+def list_users():
+    """Page with listing of users.
+
+    Can take a 'q' param in querystring to search by that username.
+    """
+
+    search = request.args.get('q')
+
+    if not search:
+        users = User.query.all()
+    else:
+        users = User.query.filter(User.username.like(f"%{search}%")).all()
+
+    return render_template('users/index.html', users=users)
+
+
 @app.route('/users/<int:user_id>')
 def users_show(user_id):
     """Show user profile."""
 
     user = User.query.get_or_404(user_id)
 
+    # snagging messages in order from the database;
+    # user.messages won't be in order by default
+    # messages = (Message
+    #             .query
+    #             .filter(Message.user_id == user_id)
+    #             .order_by(Message.timestamp.desc())
+    #             .limit(100)
+    #             .all())
     trainings = Trainings.query.filter_by(trainer_users_id=g.user.id).all()
-    return render_template('users/detail.html', user=user, trainings=trainings)
+    return render_template('users/detail.html', user=user, trainings=tr)
 
 @app.route('/users/profile', methods=["GET", "POST"])
 def profile():
@@ -157,8 +182,9 @@ def add_training():
         title = form.title.data
         description = form.description.data
         date = form.date.data
+        trainer_users_id = g.user.id
 
-        training = Trainings(title=title, description=description, date=date,trainer_users_id=g.user.id)
+        training = Trainings(title=title, description=description, date=date,trainer_users_id=trainer_users_id)
         db.session.add(training)
         db.session.commit()
 
@@ -289,7 +315,7 @@ def homepage():
         return render_template('home.html', trainers=trainers)
 
     else:
-        return render_template('login.html')
+        return redirect('/login')
 
 
 @app.after_request
